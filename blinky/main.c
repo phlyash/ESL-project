87a4871166defca7d0c70fe7821db397609366b6
Filipp <stokolovf316@gmail.com>
2024-10-28 16:58:38 +0000

    Loaded something else previously

--- blinky/main.c
+++ blinky/main.c
@@ -3,22 +3,25 @@
 #include "boards.h"
 
 #define DELAY 1000
-#define DELAY_ON_OFF 500
+#define DELAY_ON_OFF 300
 
 int main(void)
 {
 	bsp_board_init(BSP_INIT_LEDS);
 
-	uint8_t device_id = { 6, 5, 8, 0 };
+	uint8_t device_id[] = { 6, 5, 8, 0 };
 	while (true)
 	{
 		for(int i = 0; i < LEDS_NUMBER; i++)
 		{
-			bsp_board_led_invert(i);
-			nrf_delay_ms(DELAY_ON_OFF);
+			for (int j = 0; j < 2 * device_id[i]; j++) 
+			{
+				bsp_board_led_invert(i);
+				nrf_delay_ms(DELAY_ON_OFF);
+			}
+			bsp_board_led_off(i);
+			nrf_delay_ms(DELAY);
 		}
-		bsp_board_led_off(i);
-		nrf_delay_ms(DELAY);
 	}
 
 	return 0;
