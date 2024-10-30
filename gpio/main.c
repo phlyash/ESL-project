f294da2c7a0558d9254dbfbdac45d9e539744032
Filipp <stokolovf316@gmail.com>
2024-10-30 15:37:53 +0000

    gpio refactor

--- gpio/main.c
+++ gpio/main.c
@@ -2,6 +2,9 @@
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 
+#define GPIO_LED_TURN_OFF 1
+#define GPIO_LED_TURN_ON 0
+
 #define LEDS_NUMBER 4
 #define DELAY 1000
 	
@@ -13,15 +16,26 @@ uint32_t button = NRF_GPIO_PIN_MAP(1, 6);
 
 uint32_t leds[] = { NRF_GPIO_PIN_MAP(0, 6), NRF_GPIO_PIN_MAP(0, 8), NRF_GPIO_PIN_MAP(1, 9), NRF_GPIO_PIN_MAP(0, 12) };
 
+void turn_off_led(uint8_t led_idx) 
+{
+	nrf_gpio_pin_write(leds[led_idx], GPIO_LED_TURN_OFF);
+}
+
+void turn_on_led(uint8_t led_idx) 
+{
+	nrf_gpio_pin_write(leds[led_idx], GPIO_LED_TURN_ON);
+}
+
 // infinite loop if device id is { 0, 0, 0, 0 }
 // think about it better
 void is_active_led_valid()
 {
 	if (delays_passed >= device_id[active_led]) 
 	{
-		nrf_gpio_pin_write(leds[active_led], 1);
+		turn_off_led(active_led);
 		delays_passed = 0;
 		active_led = (active_led + 1) % LEDS_NUMBER;
+
 		is_active_led_valid();
 	}
 }
@@ -47,7 +61,7 @@ int main(void)
 
 		if (!nrf_gpio_pin_read(button)) 
 		{
-			nrf_gpio_pin_write(leds[active_led], 0);
+			turn_on_led(active_led);
 			nrf_delay_ms(DELAY);
 			++delays_passed;
 		}
